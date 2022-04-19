4211add951ca337004406195c42ce3f745e957f3
DaGé <a@d.a>
2022-04-19 11:49:52 +0200

    Ajout des fichiers de l'architecture du site

--- include/menu.php
+++ include/menu.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <link rel="stylesheet" href="style.css">
-    <title>Accueil de Wikifruit</title>
-</head>
-<body>
-
-<!-- Menu -->
-
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
     <div class="container-fluid">
@@ -57,7 +42,3 @@
     </div>
 
 </nav>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
--- register.php
+++ register.php
@@ -14,19 +14,6 @@
 <!-- Menu -->
 <?php include "include/menu.php"; ?>
 
-<div class="container-fluid">
-
-<div class="row">
-
-    <div class="col-12 col-md-8 mx-auto py-5">
-        <h1 class="pb-4">Accueil Wikifruit</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni repellendus hic soluta ab at ea delectus consectetur, quos suscipit, necessitatibus, impedit blanditiis! Modi fugiat accusantium repellendus impedit repellat molestias a, dolore ab praesentium recusandae quae debitis facere nesciunt eaque quasi alias iste. Explicabo ad deleniti esse aliquam est? Nam, et.</p>
-    </div>
-
-</div>
-
-</div>
-
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
